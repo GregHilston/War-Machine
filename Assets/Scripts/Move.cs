92fcca1a898e7a8643533c0f168f0d3e30245949
GregHilston <Gregory.Hilston@gmail.com>
2019-02-07 12:08:36 -0700

    initial commit of project. Just got shown how to do animation. Need to clean up everything and remove tag

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float speed;
+    private Animator anim;
+    public Transform armParent;
+
+    void Start()
+    {
+        anim = armParent.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetBool("CaughtItem", !anim.GetBool("CaughtItem"));
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        // this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(180, 0, 0), Time.deltaTime * speed);
+
+        // other.transform.rotation = this.transform.rotation;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("FIRING!!");
+        if (col.transform.tag.Equals("ConveyAble"))
+        {
+            col.transform.parent = gameObject.transform;
+            anim.SetBool("CaughtItem", true);
+        }
+    }
+}
