1564aad166e5b904d0867e0d9cf6b1b8531ffc1e
GregHilston <Gregory.Hilston@gmail.com>
2019-02-10 16:09:06 -0700

    added gismo indicating direction

--- Assets/Scripts/Convey.cs
+++ Assets/Scripts/Convey.cs
@@ -12,4 +12,18 @@ public class Convey : MonoBehaviour {
             other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
         }
     }
+
+    void OnDrawGizmos() {
+        if (endpoint != null) {
+            Gizmos.color = Color.blue;
+
+            Vector3 startPosition = transform.position;
+            startPosition.y = 1.0f;
+
+            Vector3 endPosition = endpoint.position;
+            endPosition.y = 1.0f;
+
+            Gizmos.DrawLine(startPosition, endPosition);
+        }
+    }
 }
