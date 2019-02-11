9da9b7028f6f11a76283d2b8e89f23b98aed6077
GregHilston <Gregory.Hilston@gmail.com>
2019-02-11 12:30:59 -0700

    removed unneeded launch point

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour {
-    public GameObject launchPoint;
-    public GameObject Barrel;
+    public GameObject barrel;
     public float thrust = 1000.0f;
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.GetComponent<Launchable>() != null) {
-            if (launchPoint != null) {
-                collision.gameObject.transform.position = Barrel.transform.position;
-                collision.gameObject.transform.rotation = Barrel.transform.rotation;
+            if (barrel != null) {
+                collision.gameObject.transform.position = barrel.transform.position;
+                collision.gameObject.transform.rotation = barrel.transform.rotation;
 
-                collision.gameObject.GetComponent<Rigidbody>().AddForce(Barrel.transform.up * thrust);
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(barrel.transform.up * thrust);
             }
         }
     }
