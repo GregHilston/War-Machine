ad64cf6df04809b1d220ab1757a0b03f49fe63b8
GregHilston <Gregory.Hilston@gmail.com>
2019-02-15 13:09:47 -0700

    Using require component syntax

--- Assets/Scripts/Behaviors/Launchable.cs
+++ Assets/Scripts/Behaviors/Launchable.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Launchable : MonoBehaviour {
-    public void launch(Vector3 force) { 
+    public void launch(Vector3 force) {
         this.GetComponent<Rigidbody>().AddForce(force);
     }
 }
