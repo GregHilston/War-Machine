d04ef895b10772c1276b736e6a0f798ff8503497
GregHilston <Gregory.Hilston@gmail.com>
2019-02-14 20:06:13 -0700

    Got teleporter working

---
+++ Assets/Scripts/Components/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject location;
+    public float processTime = 0.0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.GetComponent<Teleportable>() != null) {
+            collision.gameObject.transform.position = location.transform.position;
+        }
+    }
+}
