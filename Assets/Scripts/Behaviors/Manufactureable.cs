05f0ba401665498d6f1e2d56bc6a64db0b2d0cfe
GregHilston <Gregory.Hilston@gmail.com>
2019-02-16 14:25:34 -0700

    renamed Manufacture

--- Assets/Scripts/Behaviors/Manufactureable.cs
+++ Assets/Scripts/Behaviors/Manufactureable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manufactureable : MonoBehaviour {
+public class Factoryable : MonoBehaviour {
     public void transformOneObjectIntoAnother(GameObject input, GameObject output, Vector3 outputPosition, Quaternion outputRotation) {
         SimplePool.Spawn(output, outputPosition, outputRotation); // create new item before we lose reference to old one
         SimplePool.Despawn(input); // delete old one
---
+++ Assets/Scripts/Components/Factory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("What type of objects this Factory takes as input")]
+    private GameObject input;
+    [SerializeField]
+    [Tooltip("What type of objects this Factory creates as output")]
+    private GameObject output;
+    [SerializeField]
+    [Tooltip("Where this Factory should place its output")]
+    private GameObject outputStage;
+    [SerializeField]
+    [Tooltip("How long it should take to Factory")]
+    private float processTime = 0.0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        var factoryable = collision.gameObject.GetComponent<Factoryable>();
+
+        if (factoryable != null) {
+            if (collision.gameObject.GetType() == input.GetType()) {
+                factoryable.transformOneObjectIntoAnother(factoryable.gameObject, output, outputStage.transform.position, outputStage.transform.rotation);
+            }
+        }
+    }
+}
