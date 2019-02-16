b0b5fe7866abf3d9bfefcebeeffc9911554aa6f6
GregHilston <Gregory.Hilston@gmail.com>
2019-02-16 13:51:11 -0700

    Added RequireComponent where needed on behaviors

--- Assets/Scripts/Behaviors/Launchable.cs
+++ Assets/Scripts/Behaviors/Launchable.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Launchable : MonoBehaviour {
     public void launch(Vector3 force) {
-        this.GetComponent<Rigidbody>().AddForce(force);
+        var myRigidBody = this.GetComponent<Rigidbody>();
+        if (myRigidBody != null) {
+            myRigidBody.AddForce(force);
+        }
     }
 }
--- Assets/Scripts/Behaviors/Teleportable.cs
+++ Assets/Scripts/Behaviors/Teleportable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Teleportable : MonoBehaviour {
     private void zeroOutForce() {
         var myRigidBody = this.GetComponent<Rigidbody>();
