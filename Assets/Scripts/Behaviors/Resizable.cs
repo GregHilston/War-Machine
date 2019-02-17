a3d8db1a33575f11c064f41a8936ab6acab01f5d
GregHilston <Gregory.Hilston@gmail.com>
2019-02-17 10:35:13 -0700

    multiplying vector by scalar

--- Assets/Scripts/Behaviors/Resizable.cs
+++ Assets/Scripts/Behaviors/Resizable.cs
@@ -7,10 +7,6 @@ using UnityEngine;
 /// </summary>
 public class Resizable : MonoBehaviour {
     public void resize(GameObject gameObject, float multiplier) {
-        gameObject.transform.localScale = new Vector3(
-            gameObject.transform.localScale.x * multiplier,
-            gameObject.transform.localScale.y * multiplier,
-            gameObject.transform.localScale.z * multiplier
-        );
+        gameObject.transform.localScale *= multiplier;
     }
 }
