ea0f147b890e843ee26bd2ebc1b98579094f84a1
GregHilston <Gregory.Hilston@gmail.com>
2019-02-17 11:43:11 -0700

    made a fan!

---
+++ Assets/Scripts/Components/Fan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines a hitbox in front of the fan, applying a constant force on all objects that pass through.
+/// </summary>
+public class Fan : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("How much force to apply to the object we're blowing.")]
+    private float force = 1000.0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.GetComponent<Launchable>() != null) {
+            collision.gameObject.GetComponent<Launchable>().launch(gameObject.transform.up * -1.0f * force);
+        }
+    }
+}
