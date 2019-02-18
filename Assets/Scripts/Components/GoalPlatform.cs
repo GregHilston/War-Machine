0ae2b5a5fa780741cad19768bf14174c70edba88
GregHilston <Gregory.Hilston@gmail.com>
2019-02-17 21:57:02 -0700

    despawner (goal) counting correctly, but printing wrong name

--- Assets/Scripts/Components/GoalPlatform.cs
+++ Assets/Scripts/Components/GoalPlatform.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 /// Keeps track of how many of each type of item has touched it and despawned.
 /// </summary>
 public class GoalPlatform : MonoBehaviour {
-    static Dictionary<GameObject, int> despawnCount = new Dictionary<GameObject, int>();
+    static Dictionary<string, int> despawnCount = new Dictionary<string, int>();
 
     private void IncrementCount(GameObject gameObject) {
         int currentCount;
-        GoalPlatform.despawnCount.TryGetValue(gameObject, out currentCount);
-        GoalPlatform.despawnCount[gameObject] = currentCount + 1;
+        GoalPlatform.despawnCount.TryGetValue(gameObject.GetType().ToString(), out currentCount);
+        GoalPlatform.despawnCount[gameObject.GetType().ToString()] = currentCount + 1;
 
-        Debug.Log(GoalPlatform.despawnCount[gameObject] + " " + gameObject.GetType().Name + " have made it to GoalPlatform");
+        Debug.Log(GoalPlatform.despawnCount[gameObject.GetType().ToString()] + " " + gameObject.GetType().ToString() + " have made it to GoalPlatform");
     }
 
     private void OnCollisionEnter(Collision collision) {
