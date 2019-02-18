6889ecf2e65c8635a4db99085b96982a5457fb7b
GregHilston <Gregory.Hilston@gmail.com>
2019-02-17 21:30:00 -0700

    Trying to get goal platform to work

---
+++ Assets/Scripts/Behaviors/Despawnable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles despawning items.
+/// </summary>
+public class Despawnable : MonoBehaviour {
+    public void despawn() {
+        SimplePool.Despawn(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Components/GoalPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Keeps track of how many of each type of item has touched it and despawned.
+/// </summary>
+public class GoalPlatform : MonoBehaviour {
+    static Dictionary<GameObject, int> despawnCount = new Dictionary<GameObject, int>();
+
+    private void IncrementCount(GameObject gameObject) {
+        int currentCount;
+        GoalPlatform.despawnCount.TryGetValue(gameObject, out currentCount);
+        GoalPlatform.despawnCount[gameObject] = currentCount + 1;
+
+        Debug.Log(GoalPlatform.despawnCount[gameObject] + " " + gameObject.GetType().Name + " have made it to GoalPlatform");
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        var despawnable = collision.gameObject.GetComponent<Despawnable>();
+        if (despawnable != null) {
+            this.IncrementCount(despawnable.gameObject);
+
+            despawnable.despawn();
+        }
+    }
+}
