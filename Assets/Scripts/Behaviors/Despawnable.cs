5d3319ab74a0480a0ff255a351ea3e20b0ef1446
GregHilston <Gregory.Hilston@gmail.com>
2019-02-19 12:15:46 -0700

    Behaviorized/compantized the goal despawner thing

--- Assets/Scripts/Behaviors/Despawnable.cs
+++ Assets/Scripts/Behaviors/Despawnable.cs
@@ -6,7 +6,30 @@ using UnityEngine;
 /// Handles despawning items.
 /// </summary>
 public class Despawnable : MonoBehaviour {
-    public void despawn() {
+    /// <summary>
+    /// Represents all objects that have been happily despawned.
+    /// </summary>
+    private static Dictionary<string, int> happyDespawnCount = new Dictionary<string, int>();
+    /// <summary>
+    /// Represents all objects that have been angerly despawned.
+    /// </summary>
+    private static Dictionary<string, int> angryDespawnCount = new Dictionary<string, int>();
+
+    private void IncrementCount(Dictionary<string, int> dictionary) {
+        int currentCount;
+        string originalPrefabName = SimplePool.GetOriginalPrefabName(this.gameObject);
+
+        dictionary.TryGetValue(originalPrefabName, out currentCount);
+        dictionary[originalPrefabName] = currentCount + 1;
+    }
+
+    public void HappilyDespawn() {
+        this.IncrementCount(Despawnable.happyDespawnCount);
+        SimplePool.Despawn(this.gameObject);
+    }
+
+    public void AngrilyDespawn() {
+        this.IncrementCount(Despawnable.angryDespawnCount);
         SimplePool.Despawn(this.gameObject);
     }
 }
--- Assets/Scripts/Components/GoalPlatform.cs
+++ Assets/Scripts/Components/GoalPlatform.cs
@@ -7,22 +7,12 @@ using UnityEditor;
 /// Keeps track of how many of each type of item has touched it and despawned.
 /// </summary>
 public class GoalPlatform : MonoBehaviour {
-    static Dictionary<string, int> despawnCount = new Dictionary<string, int>();
 
-    private void IncrementCount(GameObject gameObject) {
-        int currentCount;
-        string originalPrefabName = SimplePool.GetOriginalPrefabName(gameObject);
-
-        GoalPlatform.despawnCount.TryGetValue(originalPrefabName, out currentCount);
-        GoalPlatform.despawnCount[originalPrefabName] = currentCount + 1;
-    }
 
     private void OnCollisionEnter(Collision collision) {
         var despawnable = collision.gameObject.GetComponent<Despawnable>();
         if (despawnable != null) {
-            this.IncrementCount(despawnable.gameObject);
-
-            despawnable.despawn();
+            despawnable.HappilyDespawn();
         }
     }
 }
