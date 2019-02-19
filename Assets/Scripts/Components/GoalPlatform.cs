67c254e3dd5523a9c753150876e69710ce952e46
GregHilston <Gregory.Hilston@gmail.com>
2019-02-19 13:12:37 -0700

    Working on UI. Got Build to tie to a function call

--- Assets/Scripts/Components/GoalPlatform.cs
+++ Assets/Scripts/Components/GoalPlatform.cs
@@ -7,8 +7,6 @@ using UnityEditor;
 /// Keeps track of how many of each type of item has touched it and despawned.
 /// </summary>
 public class GoalPlatform : MonoBehaviour {
-
-
     private void OnCollisionEnter(Collision collision) {
         var despawnable = collision.gameObject.GetComponent<Despawnable>();
         if (despawnable != null) {
