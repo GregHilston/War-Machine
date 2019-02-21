b2eed0b1db0c0df6f600df4f6c1656650dab29a9
GregHilston <Gregory.Hilston@gmail.com>
2019-02-21 12:48:45 -0700

    Added exitor and tied it to the exit button

---
+++ Assets/Scripts/Behaviors/Pausable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausable : MonoBehaviour {
+    public void PauseGame() {
+        Time.timeScale = 0;
+    }
+
+    public void ContinueGame() {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/Components/PauseGameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Pausable))]
+public class PauseGameController : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Key to press down to pause the game")]
+    string pauseKey = "p";
+    [SerializeField]
+    [Tooltip("Text to show when paused")]
+    Text pausedText;
+
+    private bool isGamePaused = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(this.pauseKey)){
+            this.isGamePaused = !this.isGamePaused;
+
+            var pauseable = GetComponent<Pausable>();
+            if (pauseable != null) {
+                if (this.isGamePaused) {
+                    pauseable.PauseGame();
+                    pausedText.gameObject.SetActive(true);
+                }
+                else {
+                    pauseable.ContinueGame();
+                    pausedText.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
