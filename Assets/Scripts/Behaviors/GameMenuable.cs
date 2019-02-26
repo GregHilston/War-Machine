07ff174819c711b8ca534a7b5beb759a52e05c2c
GregHilston <Gregory.Hilston@gmail.com>
2019-02-26 13:10:54 -0700

    making a new component for controlling when game menu is seen. Woops forgot to add when I remove pausemenu script

---
+++ Assets/Scripts/Behaviors/GameMenuable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuable : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Game Menu Parent Object to show or hide")]
+    private GameObject gameMenuParentObject;
+
+    public void ShowGameMenu() {
+        gameMenuParentObject.SetActive(true);
+    }
+
+    public void HideGameMenu() {
+        gameMenuParentObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Components/GameMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameMenuable))]
+[RequireComponent(typeof(Pausable))]
+public class GameMenuController : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Key to press down to show menu")]
+    private string gameMenuKey = "escape";
+
+
+    private bool isGameMenuBeingShown = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(this.gameMenuKey)) {
+            this.isGameMenuBeingShown = !this.isGameMenuBeingShown;
+
+            var pauseable = GetComponent<Pausable>();
+            if (pauseable != null) {
+                if (this.isGameMenuBeingShown) {
+                    pauseable.PauseGame();
+                } else {
+                    pauseable.ContinueGame();
+                }
+            }
+
+            var gameMenuable = GetComponent<GameMenuable>();
+            if (gameMenuable != null) {
+                if (this.isGameMenuBeingShown) {
+                    gameMenuable.ShowGameMenu();
+                } else {
+                    gameMenuable.HideGameMenu();
+                }
+            }
+        }
+    }
+}
