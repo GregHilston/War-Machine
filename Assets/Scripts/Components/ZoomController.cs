797a13b7f60e436028a1548b8d5d26d3d3ce2ffd
GregHilston <Gregory.Hilston@gmail.com>
2019-03-08 12:23:32 -0700

    Zoom controller not zooming if game is paused

--- Assets/Scripts/Components/ZoomController.cs
+++ Assets/Scripts/Components/ZoomController.cs
@@ -30,15 +30,21 @@ public class ZoomController : MonoBehaviour, IMouseEventRespondable {
     }
 
     public bool respoundToMouseEvent(string mouseAxis, float delta) {
-        // Handle zooming in and out
-        // From: https://answers.unity.com/questions/218347/how-do-i-make-the-camera-zoom-in-and-out-with-the.html
-        var fov = Camera.main.fieldOfView;
-        fov += delta * -1 * sensitivity;
-        fov = Mathf.Clamp(fov, minFov, maxFov);
-        Camera.main.fieldOfView = fov;
+        // since we don't use Time.deltaTime, we'll have a conditional to not zoom if the game is paused
+        float epsilon = 0.01F;
+        if(System.Math.Abs(Time.timeScale) > epsilon) {
+            // Handle zooming in and out
+            // From: https://answers.unity.com/questions/218347/how-do-i-make-the-camera-zoom-in-and-out-with-the.html
+            var fov = Camera.main.fieldOfView;
+            fov += delta * -1 * sensitivity;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            Camera.main.fieldOfView = fov;
 
-        Debug.Log("respoundToMouseEvent mouseAxis " + mouseAxis + " delta " + delta);
+            Debug.Log("respoundToMouseEvent mouseAxis " + mouseAxis + " delta " + delta);
 
-        return true;
+            return true;
+        }
+
+        return false;
     }
 }
