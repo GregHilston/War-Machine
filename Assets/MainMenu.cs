01491cc587ae822ba02ad20739a96435fb8d7979
GregHilston <Gregory.Hilston@gmail.com>
2019-03-09 10:38:18 -0700

    Wired up play and quit on main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() {
+        // When you attempt to quit in game and it doesnt quit during unity development, you can prove that it would if you built the binary
+        Debug.Log("Quit Game"); 
+        Application.Quit();
+    }
+}
