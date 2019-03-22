3bca024bda3114f7493b0551e10c42fa7f15b5e3
GregHilston <Gregory.Hilston@gmail.com>
2019-03-22 12:19:40 -0600

    Displaying lists of items

--- Assets/Scripts/LevelInformation.cs
+++ Assets/Scripts/LevelInformation.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using TMPro;
 
 public class LevelInformation : MonoBehaviour {
@@ -12,9 +13,21 @@ public class LevelInformation : MonoBehaviour {
     [SerializeField]
     private TextMeshPro badItems;
 
+    public string ItemListToString(List<LevelData.Item> items) {
+        string s = "";
+
+        foreach (LevelData.Item item in items) {
+            s += item.amount + " " + item.name + "\n";
+        }
+
+        return s;
+    }
+
     public void UpdateDisplayUI(LevelData levelData) {
-        levelName.text = levelData.LevelName;
-        levelDescription.text = levelData.Description;
+        levelName.text = "Name: " + levelData.LevelName;
+        levelDescription.text = "Description: " + levelData.Description;
+        goodItems.text = "Pass Level: " + this.ItemListToString(levelData.GoodItems);
+        badItems.text = "Fail Level: " + this.ItemListToString(levelData.BadItems);
     }
 
 }
