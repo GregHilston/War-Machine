35e7829e1c57e5c0bc66a77181e4e466c16702a7
GregHilston <Gregory.Hilston@gmail.com>
2019-03-21 21:54:32 -0600

    got text mesh pro references wired up

---
+++ Assets/Scripts/LevelInformation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelInformation : MonoBehaviour {
+    [SerializeField]
+    private TextMeshPro levelName;
+    [SerializeField]
+    private TextMeshPro levelDescription;
+    [SerializeField]
+    private TextMeshPro goodItems;
+    [SerializeField]
+    private TextMeshPro badItems;
+}
