0b2f4cd78ed69367a7403676de65799b2e6f4bac
GregHilston <Gregory.Hilston@gmail.com>
2019-03-22 23:33:39 -0600

    Got win and lose logic working

--- Assets/Scripts/Components/LevelProgress.cs
+++ Assets/Scripts/Components/LevelProgress.cs
@@ -63,6 +63,30 @@ public class LevelProgress : MonoBehaviour {
         
     }
 
+    public bool HasWonLevel() {
+        foreach (KeyValuePair<string, int> entry in this.initialGoodItems) {
+            if (this.liveGoodItems[entry.Key] < entry.Value) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool HasLostLevel() {
+        if (this.initialBadItems.Count > 0) {
+            foreach (KeyValuePair<string, int> entry in this.initialBadItems) {
+                if (this.liveBadItems[entry.Key] < entry.Value) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
     public void HandleDespawn(GameObject gameObjectAboutToBeDespawned, Despawnable.TypeOfDespawn typeOfDespawn) {
         string nameOfItemDespawned = gameObjectAboutToBeDespawned.transform.name.Split(' ')[0];
         // Debug.Log("HandleDespawn of " + nameOfItemDespawned);
@@ -71,6 +95,12 @@ public class LevelProgress : MonoBehaviour {
             this.liveGoodItems[nameOfItemDespawned] += 1;
         } else if (typeOfDespawn == Despawnable.TypeOfDespawn.Angrily) {
             this.liveBadItems[nameOfItemDespawned] += 1;
+        }
+
+        if (this.HasWonLevel()) {
+            Debug.Log("WON!");
+        } else if (this.HasLostLevel()) {
+            Debug.Log("LOST!");
         }
 
         onProgressUpdatedGameEvent.Raise();
